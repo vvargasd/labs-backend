448b28c8e305eb85db57b613751f47200fe608da
vvargasd <vvargasd@unal.edu.co>
2019-12-12 21:10:36 -0500

    haber que pasa 3

--- src/main/java/co/edu/unal/software_engineering/labs/LabsApplication.java
+++ src/main/java/co/edu/unal/software_engineering/labs/LabsApplication.java
@@ -8,7 +8,7 @@ public class LabsApplication{
 
     public static void main( String[] args ){
 
-        int a = 9/0;
+        int a = 9/(4-4);
         SpringApplication.run( LabsApplication.class, args );
     }
 
